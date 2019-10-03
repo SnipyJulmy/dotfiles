44224f10f21ee1f1f4582eec7cbdaab440decc2c
SnipyJulmy <sylvain.julmy@gmail.com>
2019-10-03 16:33:36 +0200

    Add a function install to simplify the installation

--- install.sh
+++ install.sh
@@ -4,47 +4,73 @@
 # so one can just modify the files from the .dotfiles directory
 
 DO_BACKUP=false
+OVERWRITE=false
 
-VIMRC="$HOME/.vimrc"
+LOCAL_VIMRC=".vimrc"
+DEST_VIMRC="$HOME/.vimrc"
+
 SPACEMACS="$HOME/.spacemacs"
 NVIMRC="$HOME/.config/nvim/init.vim"
 TMUX="$HOME/.tmux.conf"
+CTAGS="$HOME/.ctags"
 
-if [[ -e $VIMRC ]]; then
-	if [[ $DO_BACKUP ]]; then
-		mv $VIMRC "$VIMRC.bak"
-	else
-		rm $VIMRC
-	fi
-	ln -s ~/.dotfiles/.vimrc $VIMRC
-fi
+# install a file into the specified path, the installation is done by creating symbolic link
+# so one can modify standard file and modify only the one present in the git repository
+# expect # arguments :
+# #1 : the file name present in the git repository
+# #2 : the standard path where the file should be put
+function install() {
+    SOURCE_PATH=`realpath $1`
+    DESTINATION_PATH=$2
+    if [[ -e $DESTINATION_PATH ]]; then
+        if [[ $OVERWRITE == true ]]; then
+            if [[ $DO_BACKUP == true ]]; then
+                echo "mv $DESTINATION_PATH $DESTINATION_PATH.bak"
+            fi
+            echo "rm $DESTINATION_PATH"
+        fi
+    fi
+    echo "ln -s $SOURCE_PATH $DESTINATION_PATH"
+}
 
-if [[ -e $SPACEMACS ]]; then
-	if [[ $DO_BACKUP ]]; then
-		mv $SPACEMACS "$SPACEMACS.bak"
-	else
-		rm $SPACEMACS
-	fi
-	rm $SPACEMACS
-	ln -s ~/.dotfiles/.spacemacs $SPACEMACS
-fi
+install $LOCAL_VIMRC $DEST_VIMRC
 
-if [[ -e $NVIMRC ]]; then
-	if [[ $DO_BACKUP ]]; then
-		mv $NVIMRC "$NVIMRC.bak"
-	else
-		rm $NVIMRC
-	fi
-	rm $NVIMRC
-	ln -s ~/.dotfiles/init.vim $NVIMRC
-fi
-
-if [[ -e $TMUX ]]; then
-	if [[ $DO_BACKUP ]]; then
-		mv $TMUX "$TMUX.bak"
-	else
-		rm $TMUX
-	fi
-	ln -s ~/.dotfiles/.tmux.conf $TMUX
-fi
-
+#
+#if [[ -e $VIMRC ]] && [[ $OVERWRITE ]] ; then
+#	if [[ $DO_BACKUP ]]; then
+#		mv $VIMRC "$VIMRC.bak"
+#	else
+#		rm $VIMRC
+#	fi
+#fi
+#
+#if [[ -e $SPACEMACS ]] && [[ $OVERWRITE ]] ; then
+#	if [[ $DO_BACKUP ]]; then
+#		mv $SPACEMACS "$SPACEMACS.bak"
+#	else
+#		rm $SPACEMACS
+#	fi
+#	rm $SPACEMACS
+#fi
+#
+#if [[ -e $NVIMRC ]] && [[ $OVERWRITE ]] ; then
+#	if [[ $DO_BACKUP ]]; then
+#		mv $NVIMRC "$NVIMRC.bak"
+#	else
+#		rm $NVIMRC
+#	fi
+#fi
+#
+#if [[ -e $TMUX ]] && [[ $OVERWRITE ]] ; then
+#	if [[ $DO_BACKUP ]]; then
+#		mv $TMUX "$TMUX.bak"
+#	else
+#		rm $TMUX
+#	fi
+#fi
+#
+#ln -s ~/.dotfiles/.vimrc $VIMRC
+#ln -s ~/.dotfiles/.tmux.conf $TMUX
+#ln -s ~/.dotfiles/init.vim $NVIMRC
+#ln -s ~/.dotfiles/.spacemacs $SPACEMACS
+#ln -s ~/.dotfiles/.ctags $CTAGS
