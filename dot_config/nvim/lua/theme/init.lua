47696da241f0a13b98f58eabe0a49e450a7f72a4
Sylvain (Snipy) Julmy <sylvain.julmy@gmail.com>
2022-11-07 16:40:44 +0100

    Update dot_config/nvim/init.lua Update dot_config/nvim/lua/autocommands.lua Update dot_config/nvim/lua/global.lua Update dot_config/nvim/lua/options.lua Update dot_config/nvim/lua/plugins.lua Update dot_config/nvim/lua/settings/dap.lua Update dot_config/nvim/lua/settings/lsp.lua Update dot_config/nvim/lua/settings/lualine.lua Update dot_config/nvim/lua/settings/mappings.lua Update dot_config/nvim/lua/settings/neo-tree.lua Update dot_config/nvim/lua/settings/nvim-cmp.lua Update dot_config/nvim/lua/settings/telescope.lua Update dot_config/nvim/lua/settings/treesitter.lua Update dot_config/nvim/lua/settings/which-key.lua Update dot_config/nvim/lua/theme/init.lua Update dot_config/nvim/lua/utils/init.lua

--- dot_config/nvim/lua/theme/init.lua
+++ dot_config/nvim/lua/theme/init.lua
@@ -1,8 +1,1 @@
-vim.g.catppuccin_flavour = "macchiato"
-require("catppuccin").setup()
-vim.api.nvim_command([[colorscheme gruvbox]])
-vim.cmd([[highlight Normal guibg=None]])
-vim.cmd([[highlight WinSeparator guibg=None]])
-vim.go.guicursor = nil
-vim.opt.laststatus = 3 -- global statusline
--- vim.opt.cmdheight = 1 -- height of below command status line
+
