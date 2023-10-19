13feb87a526dc4b7fa7090449b4cb43f4252966c
Sylvain Julmy (IMTF) <sylvain.julmy@imtf.com>
2023-10-19 10:07:16 +0200

    Update home/.config/nvim/ftplugin/lua.lua

--- home/dot_config/nvim/ftplugin/lua.lua
+++ home/dot_config/nvim/ftplugin/lua.lua
@@ -1,6 +1,6 @@
 local lua_group = vim.api.nvim_create_augroup("Lua", { clear = true })
 
-vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
   group = lua_group,
   pattern = { "*.lua" },
   callback = function(args)
