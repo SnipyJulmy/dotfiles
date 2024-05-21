9d11bdd7545cccf00f4fb018bf31937556a71a6d
Sylvain Julmy (IMTF) <sylvain.julmy@imtf.com>
2024-05-21 12:10:12 +0200

    Add .github/workflows/ci.yml Update .github/workflows/gitleaks.yml Update home/.config/nvim/init.lua Update home/.config/nvim/lazy-lock.json Update home/.config/nvim/lua/snipy/git.lua Update home/.config/nvim/lua/snipy/plugins/git.lua

--- home/dot_config/nvim/lua/snipy/plugins/git.lua
+++ home/dot_config/nvim/lua/snipy/plugins/git.lua
@@ -9,15 +9,41 @@ return {
       require("litee.gh").setup()
     end,
   },
-  "tpope/vim-fugitive",
-  { "NeogitOrg/neogit", branch = "nightly", dependencies = { "nvim-lua/plenary.nvim" }, config = true },
-  "lewis6991/gitsigns.nvim",
+  {
+    "tpope/vim-fugitive",
+    config = function()
+      vim.keymap.set({ "n" }, "<Space>gb", "<cmd>Git blame<CR>")
+    end,
+  },
+  {
+    "NeogitOrg/neogit",
+    branch = "master",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("neogit").setup({})
+      vim.keymap.set({ "n" }, "<Space>gg", require("neogit").open)
+    end,
+  },
+  {
+    "lewis6991/gitsigns.nvim",
+    config = function()
+      require("gitsigns").setup({
+        signs = {
+          add = { text = "+" },
+          change = { text = "~" },
+          deleted = { text = "-" },
+        },
+        numhl = true,
+      })
+    end,
+    vim.keymap.set({ "n" }, "B", "<cmd>Gitsigns blame_line<CR>"),
+  },
   {
     "rhysd/git-messenger.vim",
     config = function()
       vim.g.git_messenger_no_default_mappings = true
+      vim.keymap.set({ "n" }, "M", "<cmd>GitMessenger<CR>")
     end,
   },
   "rhysd/committia.vim",
-  "sindrets/diffview.nvim",
 }
