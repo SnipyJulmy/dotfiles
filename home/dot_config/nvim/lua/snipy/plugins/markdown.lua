d5b631cbcd5a186357b9748d99e3fcb277673e17
Sylvain (Snipy) Julmy <sylvain.julmy@gmail.com>
2024-11-03 16:14:16 +0100

    Update home/.config/nvim/lua/snipy/plugins/markdown.lua

--- home/dot_config/nvim/lua/snipy/plugins/markdown.lua
+++ home/dot_config/nvim/lua/snipy/plugins/markdown.lua
@@ -14,11 +14,17 @@ return {
     config = function()
       require("render-markdown").setup({
         file_types = { "markdown", "vimwiki" },
+        enabled = true,
         latex = {
           enabled = false,
         },
       })
       vim.treesitter.language.register("markdown", "vimwiki")
+      vim.keymap.set({ "n" }, "<leader>mm", require("render-markdown").toggle)
+      vim.keymap.set({ "n" }, "<leader>me", require("render-markdown").enable)
+      vim.keymap.set({ "n" }, "<leader>md", require("render-markdown").disable)
+      vim.keymap.set({ "n" }, "<leader>mj", require("render-markdown").expand)
+      vim.keymap.set({ "n" }, "<leader>mk", require("render-markdown").contract)
     end,
   },
 }
