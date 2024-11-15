0acc8f31d10c59076c098b1b22347616323a3392
Sylvain Julmy (IMTF) <sylvain.julmy@imtf.com>
2024-11-15 09:13:22 +0100

    Update home/.config/nvim/lua/snipy/plugins/conform.lua Update home/.config/nvim/lua/snipy/plugins/heirline.lua Update home/.zshrc

--- home/dot_config/nvim/lua/snipy/plugins/heirline.lua
+++ home/dot_config/nvim/lua/snipy/plugins/heirline.lua
@@ -1,11 +1,15 @@
 return {
   {
     "rebelot/heirline.nvim",
+    enabled = true,
     config = function()
+      local statusline = {}
+      local winbar = {}
+      local tabline = {}
       require("heirline").setup({
-        statusline = {},
-        winbar = {},
-        tabline = {},
+        statusline = statusline,
+        winbar = winbar,
+        tabline = tabline,
         opts = {
           disable_winbar_cb = function()
             return true
