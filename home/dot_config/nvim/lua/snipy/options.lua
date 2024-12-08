2817dcaecad774e646270454de0b1f3216737da8
Sylvain (Snipy) Julmy <sylvain.julmy@gmail.com>
2024-12-08 12:30:42 +0100

    Update home/.config/espanso/match/french.yml Update home/.config/nvim/lazy-lock.json Update home/.config/nvim/lua/snipy/options.lua

--- home/dot_config/nvim/lua/snipy/options.lua
+++ home/dot_config/nvim/lua/snipy/options.lua
@@ -16,7 +16,7 @@ vim.o.completeopt = "menuone,noselect"
 -- vim.g.python_host_prog = os.getenv("HOME") .. "/.pyenv/versions/neovim2/bin/python"
 -- vim.g.python_host_prog = os.getenv("HOME") .. "/.pyenv/versions/neovim3/bin/python"
 
-vim.opt.mouse = "" -- disable mouse
+vim.opt.mouse = "n" -- enable mouse in normal mode only
 
 vim.opt.expandtab = true -- tabs are spaces
 vim.opt.smarttab = false
