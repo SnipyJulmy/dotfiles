eefe27ec9285d8ed6d025c750406b15fa6d635ec
Sylvain Julmy (IMTF) <sylvain.julmy@imtf.com>
2024-12-23 15:25:25 +0100

    Update home/.config/nvim/init.lua Update home/.config/nvim/lua/snipy/plugins/snipe.lua

--- home/dot_config/nvim/lua/snipy/plugins/snipe.lua
+++ home/dot_config/nvim/lua/snipy/plugins/snipe.lua
@@ -10,6 +10,31 @@ return {
         desc = "Open Snipe buffer menu",
       },
     },
-    opts = {},
+    opts = {
+      ui = {
+        max_height = -1,
+        position = "cursor",
+        open_win_override = {
+          border = "rounded",
+        },
+        preselect_current = false,
+        preselect = nil,
+        text_align = "left",
+      },
+      hints = {
+        dictionary = "asdfghjklqweruiop",
+      },
+      navigate = {
+        next_page = "J",
+        prev_page = "K",
+        under_cursor = "<cr>",
+        cancel_snipe = "<esc>",
+        close_buffer = "D",
+        open_vsplit = "V",
+        open_split = "H",
+        change_tag = "C",
+      },
+      sort = "default",
+    },
   },
 }
