8228c135c7a4e4ae714fa247011d699e915ad796
Sylvain Julmy (IMTF) <sylvain.julmy@imtf.com>
2025-02-05 10:07:27 +0100

    Update home/.config/nvim/lua/snipy/plugins/conform.lua

--- home/dot_config/nvim/lua/snipy/plugins/conform.lua
+++ home/dot_config/nvim/lua/snipy/plugins/conform.lua
@@ -21,7 +21,7 @@ return {
         -- zsh = { "beautysh" },
         js = { "prettier" },
         json = { "fixjson" },
-        yaml = { "yamllint" },
+        -- yaml = { "yamllint" },
         terraform = { "terraform_fmt" },
         rust = { "rustfmt" },
         go = { "gofmt" },
