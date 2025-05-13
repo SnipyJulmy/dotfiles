14e778b9cf5f1eec125ff83571363042ea0f801b
Sylvain Julmy (IMTF) <sylvain.julmy@imtf.com>
2025-05-13 08:49:28 +0200

    cleaning

--- home/dot_config/nvim/ftplugin/go.lua
+++
@@ -1,7 +0,0 @@
-local gofmt = vim.api.nvim_create_augroup("GoFormat", { clear = true })
-
-vim.api.nvim_create_autocmd({ "BufWritePre" }, {
-  group = gofmt,
-  pattern = "*.go",
-  callback = require("go.format").gofmt,
-})
