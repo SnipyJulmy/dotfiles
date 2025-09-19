94b7ffb97aba2c09bed8387160ba215d459a82a3
Sylvain Julmy (IMTF) <sylvain.julmy@imtf.com>
2025-09-19 10:36:44 +0200

    Add .config/home-manager/home.nix Remove .config/starship.toml

---
+++ home/dot_config/home-manager/home.nix
@@ -0,0 +1,68 @@
+{ config, pkgs, ... }:
+
+let
+  unstable = import <unstable> { };
+in
+
+{
+  home.username = "snipy";
+  home.homeDirectory = "/home/snipy";
+  home.stateVersion = "24.11";
+
+  home.packages = [
+    pkgs.age
+    pkgs.bat
+    pkgs.conky
+    pkgs.curl
+    pkgs.eza
+    pkgs.fastfetch
+    pkgs.fd
+    pkgs.gh
+    pkgs.ghostty
+    pkgs.gnutar
+    pkgs.gzip
+    pkgs.htop
+    pkgs.jira-cli-go
+    pkgs.jq
+    pkgs.lazydocker
+    pkgs.lazygit
+    pkgs.pass
+    pkgs.polybar
+    pkgs.ripgrep
+    pkgs.ungoogled-chromium
+    pkgs.unzip
+    pkgs.xclip
+    pkgs.yazi
+    pkgs.yubikey-manager
+    pkgs.yubikey-manager-qt
+    pkgs.zellij
+    pkgs.zk
+
+    unstable.k9s
+    unstable.chezmoi
+    unstable.rofi
+    unstable.tree-sitter
+    unstable.eww
+  ];
+
+  programs.git = {
+    enable = true;
+  };
+
+  programs.starship = {
+    enable = true;
+    settings = {
+      character = {
+        success_symbol = "[λ](bold green)";
+        error_symbol = "[λ](bold red)";
+        vicmd_symbol = "[λ](bold blue)";
+      };
+    };
+  };
+
+  home.file = { };
+
+  home.sessionVariables = { };
+
+  programs.home-manager.enable = true;
+}
